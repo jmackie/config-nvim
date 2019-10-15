8c1d826e708cda7be722cbb8031f17adae93c7ad
Jordan Mackie <git@jmackie.dev>
2019-10-15 13:08:24 +0100

    Add webpack and elm templates

--- templates/skeleton.elm
+++ templates/skeleton.elm
@@ -1,1 +1,59 @@
--- TODO
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events as Events
+
+
+type alias Model =
+    { count : Int }
+
+
+initialModel : Model
+initialModel =
+    { count = 0 }
+
+
+type Msg
+    = Increment
+
+
+main : Program flags Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm template"
+    , body =
+        [ Html.button
+            [ Attr.class "button"
+            , Events.onClick PlayButtonClicked
+            ]
+            [ Html.text (String.fromInt model.count) ]
+        ]
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( { model | count = model.count + 1 }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
--- templates/skeleton.webpack.config.js
+++ templates/skeleton.webpack.config.js
@@ -1,1 +1,28 @@
-// TODO
+const path = require("path");
+
+// https://github.com/jantimon/html-webpack-plugin#options
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
